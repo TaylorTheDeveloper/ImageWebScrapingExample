23f2734091d06dda1c7ca846357b763f84ce2082
Taylor Brockhoeft <taylorbrockhoeft@outlook.com>
2023-07-02 00:55:49 -0400

    adds pdf generation, license, code cleanup

---
+++ Images.cs
@@ -0,0 +1,39 @@
+﻿using HtmlAgilityPack;
+using System.Drawing;
+
+namespace ImageWebScrapingExample
+{
+    public static class Images
+    {
+        public static async Task DownloadImages(string outputFolder, string url, string includeFilter)
+        {
+            Directory.CreateDirectory(outputFolder);
+            var web = new HtmlWeb();
+            var doc = await web.LoadFromWebAsync(url);
+            int page = 1;
+
+            var httpClient = new HttpClient();
+
+            foreach (var link in doc.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                string imageUrl = link.GetAttributeValue("src", null);
+                if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+                    imageUrl = new Uri(new Uri(url), imageUrl).AbsoluteUri;
+
+                if (!imageUrl.Contains(includeFilter))
+                    continue;
+
+                var imageData = await httpClient.GetByteArrayAsync(imageUrl);
+                using (var ms = new MemoryStream(imageData))
+                {
+                    var image = Image.FromStream(ms);
+
+                    string filename = Path.Combine(outputFolder, outputFolder + "-page" + page.ToString().PadLeft(3, '0') + ".jpeg");
+                    File.WriteAllBytes(filename, imageData);
+                    Console.WriteLine($"Saved {filename}");
+                    page++;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,52 +1,25 @@
-﻿using HtmlAgilityPack;
-using System.Drawing;
-
-namespace ComicRebuilder
+﻿namespace ImageWebScrapingExample
 {
     internal class Program
     {
-        // This example shows how one might use HTMLAgilityPack to automate the aquisition of image content from a web page.
+        // This educational example shows how one might use HTMLAgilityPack to automate the aquisition of image content from a web page and assemble a PDF using C#.
         static async Task Main(string[] args)
         {
+            // Filering images is really useful and how you filter images will depend on the site, and your collection use case, etc.
+            // In this case, I am filtering using the expected hostname for the image CDN. You can filter based of this, the file name, the image size, etc.
             string includeFilter = "2.bp.blogspot.com";
+            string title = "futurama-comics";
+            string host = "https://viewcomics.me/";
 
-            for (int i=1; i<84; i++)
+            int issues = 1;
+
+            for (int i = 1; i < issues + 1; i++)
             {
                 string issue = "issue-" + i.ToString();
-                string endpoint = "https://viewcomics.me/futurama-comics/" + issue + "/full";
-                await DownLoadComic(issue, endpoint, includeFilter);
+                string endpoint = host + title +"/" + issue + "/full";
+                await Images.DownloadImages(issue, endpoint, includeFilter);
+                PDF.CreatePdfFromImages(title, issue);
             }            
-        }
-
-        static async Task DownLoadComic(string outputFolder, string url, string includeFilter)
-        {
-            Directory.CreateDirectory(outputFolder);
-            var web = new HtmlWeb();
-            var doc = await web.LoadFromWebAsync(url);
-            int page = 1;
-
-            var httpClient = new HttpClient();
-
-            foreach (var link in doc.DocumentNode.SelectNodes("//img[@src]"))
-            {
-                string imageUrl = link.GetAttributeValue("src", null);
-                if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
-                    imageUrl = new Uri(new Uri(url), imageUrl).AbsoluteUri;
-
-                if (!imageUrl.Contains(includeFilter))
-                    continue;
-
-                var imageData = await httpClient.GetByteArrayAsync(imageUrl);
-                using (var ms = new MemoryStream(imageData))
-                {
-                    var image = Image.FromStream(ms);
-
-                    string filename = Path.Combine(outputFolder, outputFolder + "-page" + page.ToString() + ".jpeg");
-                    File.WriteAllBytes(filename, imageData);
-                    Console.WriteLine($"Saved {filename}");
-                    page++;
-                }
-            }
         }
     }
 }
