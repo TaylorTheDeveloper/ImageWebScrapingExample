4f114229acddd589a08d5f92ba50e381b0eef1f6
Taylor Brockhoeft <taylorbrockhoeft@outlook.com>
2023-07-02 01:03:34 -0400

    cleanup, adds PDF example, adds License

---
+++ ImageWebScrapingExample/PDF.cs
@@ -0,0 +1,33 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ImageWebScrapingExample
+{
+    public static class PDF
+    {
+        public static void CreatePdfFromImages(string title, string folder)
+        {
+            string[] imageFiles = Directory.GetFiles(folder, "*.jpeg");
+            Array.Sort(imageFiles);
+
+            using (var stream = new FileStream(Path.Combine(folder, title + "-" + folder + ".pdf"), FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                var document = new iTextSharp.text.Document();
+
+                var writer = PdfWriter.GetInstance(document, stream);
+                document.Open();
+
+                foreach (var imageFile in imageFiles)
+                {
+                    var image = Image.GetInstance(imageFile);
+                    document.SetPageSize(new Rectangle(0, 0, image.Width, image.Height, 0));
+                    document.NewPage();
+                    image.SetAbsolutePosition(0, 0);
+                    writer.DirectContent.AddImage(image);
+                }
+
+                document.Close();
+            }
+        }
+    }
+}
---
+++ ImageWebScrapingExample/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ImageWebScrapingExample
+{
+    internal class Program
+    {
+        // This educational example shows how one might use HTMLAgilityPack to automate the aquisition of image content from a web page and assemble a PDF using C#.
+        static async Task Main(string[] args)
+        {
+            // Filering images is really useful and how you filter images will depend on the site, and your collection use case, etc.
+            // In this case, I am filtering using the expected hostname for the image CDN. You can filter based of this, the file name, the image size, etc.
+            string includeFilter = "2.bp.blogspot.com";
+            string title = "futurama-comics";
+            string host = "https://viewcomics.me/";
+
+            int issues = 1;
+
+            for (int i = 1; i < issues + 1; i++)
+            {
+                string issue = "issue-" + i.ToString();
+                string endpoint = host + title +"/" + issue + "/full";
+                await Images.DownloadImages(issue, endpoint, includeFilter);
+                PDF.CreatePdfFromImages(title, issue);
+            }            
+        }
+    }
+}
